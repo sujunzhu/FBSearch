2d1b60c2704ae5e2d42c04d0a0ef15dd516f788a
Sujun Zhu <sujunzhu@usc.edu>
2017-11-30 06:00:34 -0800

    new

--- main.php
+++ main.php
@@ -4,7 +4,7 @@
 	$fb = new Facebook\Facebook([
   				'app_id' => '1845517239059399',
   				'app_secret' => 'de8e7c04bbae8e7df7e456fe4137e145',
-  				'default_access_token' => 'EAAX5TMSkNN0BACGRbuZAMRDBWMHmclITuxvkgRGGzdw0CtcxMMVcxsZCkZBpcG1LFU8Cqxxa6UYG2A9h4kfMxnfOnqFVGO47nxRpM3UBuCq7wrwSGCNcC4kc2PDqfJ3LP4vreu69cggNuUOzsRiaiYgZCgWYrLwOg2Jfc8GGZBQaDEHNXkTWFYA7ypqGZA9aIZD',
+  				'default_access_token' => 'EAAaOfPd2A8cBAJYTbmwsXGrdtgCJe9LZB1QFig6Du6eKjUHlNlM9XPaVP9LNmkoSufAqnZBIpyKVlc03LfwLpF8W6XZALyZA5XQuPy1VtTIZBbgo8KaxmKWhZBnVvZBjtVJUE5MErYBV1NQFZAXr0epLQ4VIwZCG9Vi4ZD',
   				'default_graph_version' => 'v2.8',
 			]);
 	header('Content-type:application/json');
